2ab8d3d975fcc3c87d41a7609d3bdf3127d470e7
IaroslavTitov <iaroslav.titov@gmail.com>
2025-04-27 16:27:55 -0600

    Updating skype -> github in contacts, fixing deploy script

--- Portfolio2.0/Deploy.bat
+++
@@ -1,5 +0,0 @@
-aws ecr get-login-password --region us-east-1 --profile personal | docker login --username AWS --password-stdin 293046760863.dkr.ecr.us-east-1.amazonaws.com
-docker build -t iaroportfolio .
-docker tag iaroportfolio:latest 293046760863.dkr.ecr.us-east-1.amazonaws.com/iaroportfolio:latest
-docker push 293046760863.dkr.ecr.us-east-1.amazonaws.com/iaroportfolio:latest
-PAUSE
---
+++ Portfolio2.0/Deploy.sh
@@ -0,0 +1,4 @@
+aws ecr get-login-password --region us-east-1 --profile default | docker login --username AWS --password-stdin 293046760863.dkr.ecr.us-east-1.amazonaws.com
+docker build -t iaroportfolio .
+docker tag iaroportfolio:latest 293046760863.dkr.ecr.us-east-1.amazonaws.com/iaroportfolio:latest
+docker push 293046760863.dkr.ecr.us-east-1.amazonaws.com/iaroportfolio:latest
